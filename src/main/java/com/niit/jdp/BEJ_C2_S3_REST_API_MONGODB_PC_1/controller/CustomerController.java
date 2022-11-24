eeffa42d017ba9049221fc375db7640289bda299
Praveen Kumar <praveen170596@gmail.com>
2022-11-24 16:31:23 +0530

    created constructor for CustomerService class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/controller/CustomerController.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/controller/CustomerController.java
@@ -7,5 +7,16 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.controller;
 
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.service.CustomerService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("customerdata/api/")
 public class CustomerController {
+    private CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
 }
