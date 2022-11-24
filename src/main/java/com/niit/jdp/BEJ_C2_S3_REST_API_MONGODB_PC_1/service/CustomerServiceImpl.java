575dada12000d70fcf8295eac553cde90db92e61
Praveen Kumar <praveen170596@gmail.com>
2022-11-24 16:18:24 +0530

    created CustomerController class

--- src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/service/CustomerServiceImpl.java
+++ src/main/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/service/CustomerServiceImpl.java
@@ -7,5 +7,40 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.service;
 
-public class CustomerServiceImpl {
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Customer;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerServiceImpl(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @Override
+    public Customer saveCustomer(Customer customer) {
+        return customerRepository.save(customer);
+    }
+
+    @Override
+    public List<Customer> getAllCustomerData() {
+        return customerRepository.findAll();
+    }
+
+    @Override
+    public boolean deleteCustomer(int customerId) {
+        return true;
+    }
+
+    @Override
+    public List<Customer> getAllCustomerByProductName(String productName) {
+        return customerRepository.findAllCustomerFromProductName(productName);
+    }
 }
