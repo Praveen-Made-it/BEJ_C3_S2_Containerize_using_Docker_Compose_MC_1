557a0d73a66cc5e22ba6b7d37a4e37f5f6d65bef
Praveen Kumar <praveen170596@gmail.com>
2022-11-25 11:58:38 +0530

    Test case written for saving customer object

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerRepositoryTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerRepositoryTest.java
@@ -1,20 +1,47 @@
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.repository;
 
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Customer;
+import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Product;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
+@DataMongoTest
 class CustomerRepositoryTest {
+    @Autowired
+    private CustomerRepository customerRepository;
+    private Product product;
+    private Customer customer;
 
     @BeforeEach
     void setUp() {
+        product = new Product(1, "Apple", "Laptop");
+        customer = new Customer(1, "praveen", "9092222610", product);
     }
 
     @AfterEach
     void tearDown() {
+        product = null;
+        customer = null;
+        customerRepository.deleteAll();
     }
 
     @Test
-    void findAllCustomerFromProductName() {
+        //Test case for saving customer object
+    void giveCustomerToSaveReturnSavedCustomer() {
+        customerRepository.save(customer);
+        Customer customer1 = customerRepository.findById(customer.getCustomerId()).get();
+        assertNotNull(customer1);
+        assertEquals(customer.getCustomerId(), customer1.getCustomerId());
     }
+
+
 }
