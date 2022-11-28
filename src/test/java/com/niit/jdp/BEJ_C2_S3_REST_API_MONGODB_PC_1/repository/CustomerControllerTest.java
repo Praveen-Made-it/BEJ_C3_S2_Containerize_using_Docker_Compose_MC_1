2dfd5117854dd861762655e9978811dbbfcf3376
Praveen Kumar <praveen170596@gmail.com>
2022-11-28 14:31:21 +0530

    test cases written for Controller layer

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerControllerTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerControllerTest.java
@@ -7,44 +7,97 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.repository;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.controller.CustomerController;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Customer;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.domain.Product;
 import com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.service.CustomerServiceImpl;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @ExtendWith(MockitoExtension.class)
 public class CustomerControllerTest {
-    List<Customer> customerList;
+
     @Autowired
     private MockMvc mockMvc;
+
     @Mock
     private CustomerServiceImpl customerService;
+
     @InjectMocks
     private CustomerController customerController;
     private Customer customer;
     private Product product;
+    List<Customer> customerList;
+
+    private static String jsonToString(final Object ob) throws JsonProcessingException {
+        String result;
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            String jsonContent = mapper.writeValueAsString(ob);
+            result = jsonContent;
+        } catch (JsonProcessingException e) {
+            result = "JSON processing error";
+        }
+
+        return result;
+    }
 
     @BeforeEach
     void setUp() {
-        product = new Product(1011, "dairy Milk", "chocolate");
-        customer = new Customer(101, "Praveen", "9092222610", product);
+        product = new Product(41, "Product41", "ProductDescription41");
+        customer = new Customer(41, "Arjun41", "9092222610", product);
         mockMvc = MockMvcBuilders.standaloneSetup(customerController).build();
 
     }
 
     @AfterEach
     void tearDown() {
+        product = null;
         customer = null;
+
     }
+
+    @Test
+    public void saveCustomerTestSuccess() throws Exception {
+        when(customerService.saveCustomer(any())).thenReturn(customer);
+        mockMvc.perform(post("/customerdata/api/customer")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToString(customer)))
+                .andExpect(status().isCreated()).andDo(MockMvcResultHandlers.print());
+        verify(customerService, times(1)).saveCustomer(any());
+
+    }
+
+    @Test
+    public void deleteCustomerSuccess() throws Exception {
+        when(customerService.deleteCustomer(anyInt())).thenReturn(true);
+        mockMvc.perform(delete("/customerdata/api/customer/41")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonToString(customer)))
+                .andExpect(status().is2xxSuccessful())
+                .andDo(MockMvcResultHandlers.print());
+        verify(customerService, times(1)).deleteCustomer(anyInt());
+
+    }
+
 }
