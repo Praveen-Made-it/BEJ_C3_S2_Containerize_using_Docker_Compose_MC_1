971f2aed450627f3817ac0f217f79ea260b9d34d
Praveen Kumar <praveen170596@gmail.com>
2022-11-28 13:58:17 +0530

    added @ExtendWith(MockitoExtension.class)

--- src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerControllerTest.java
+++ src/test/java/com/niit/jdp/BEJ_C2_S3_REST_API_MONGODB_PC_1/repository/CustomerControllerTest.java
@@ -7,5 +7,10 @@
 
 package com.niit.jdp.BEJ_C2_S3_REST_API_MONGODB_PC_1.repository;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomerControllerTest {
+
 }
